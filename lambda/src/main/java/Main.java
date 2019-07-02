567d861fc52835df48849dc26a5f1c4fbd25921d
coderfocus <songwenjie830@gmail.com>
2019-07-02 10:59:41 +0800

    feat(Java8): 新增stream

--- lambda/src/main/java/Main.java
+++ lambda/src/main/java/Main.java
@@ -4,8 +4,10 @@ import transfercode.FilterApples;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -33,6 +35,12 @@ public class Main {
         List<Apple> greenApples1 = FilterApples.filterApples(apples,(Apple a) -> "green".equals(a.getColor()));
         List<Apple> heavyApples1 = FilterApples.filterApples(apples, a-> a.getWeight() >150);
         List<Apple> greenAndHeavyApples = FilterApples.filterApples(apples, a-> "green".equals(a.getColor()) && a.getWeight() >150);
+
+        //stream 筛选数据
+        List<Apple> streamApples = apples.stream()
+                .filter(a->a.getWeight()>150)
+                .collect(Collectors.toList());
+
 
     }
 }
